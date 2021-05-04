496d913d57532b8364a62b61bb3ecafa5828d7d3
edoardotavilla <edoardo.tavilla@gmail.com>
2021-05-04 15:36:05 +0200

    First setup (blank structure)

---
+++ A3/src/it/unipr/ds/A3/Client.java
@@ -0,0 +1,10 @@
+package it.unipr.ds.A3;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ A3/src/it/unipr/ds/A3/Coordinator.java
@@ -0,0 +1,9 @@
+package it.unipr.ds.A3;
+
+public class Coordinator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
